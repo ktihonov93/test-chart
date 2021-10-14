cf6d63094446f065f213aadb9566faf3a35480ad
ktihonov93 <k_tihonov@mail.ru>
2021-10-14 13:36:59 +0300

    add min height for colomns

--- src/Components/Graph.jsx
+++ src/Components/Graph.jsx
@@ -7,9 +7,9 @@ function Graph({ column, name, maxSum }){
 
     return (
         <div className="graph">
-            <div id="front" style={{height: column.front + "px"}}>{column.front}</div>
-            <div id="back" style={{height: column.back + "px"}}>{column.back}</div>
-            <div id="db" style={{height: column.db + "px"}}>{column.db}</div>
+            <div id="front" style={{height: /*column>15?*/ column.front*400/maxSum /*: 15*/ + "px"}}>{column.front}</div>
+            <div id="back" style={{height: /*column>15?*/ column.back*400/maxSum /*: 15*/ + "px"}}>{column.back}</div>
+            <div id="db" style={{height: /*column>15?*/ column.db*400/maxSum /*: 15*/ + "px"}}>{column.db}</div>
             <div className="graphName">{name}</div>
         </div>
     )
--- src/Components/Normativ.jsx
+++ src/Components/Normativ.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import './Normativ.css';
 
-function Normativ({ column, name }){
+function Normativ({ column, name, maxSum }){
 
     return (
         <div className="Normativ">
-            <div id="norm" style={{height: column + "px"}}>{column}</div>
+            <div id="norm" style={{height: /*column>15? */column*400/maxSum/* : 15*/ + "px"}}>{column}</div>
             <div className="graphName">{name}</div>
         </div>
     )
